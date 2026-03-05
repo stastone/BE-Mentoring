b84895a48833622ba2d7ba2427c77ca4bdf047fc
stastone <StanislauMarudau@coherentsolutions.com>
2026-03-05 22:52:40 +0200

    feat: resolve TS errors

--- src/middlewares/restrictTo.ts
+++ src/middlewares/restrictTo.ts
@@ -1,10 +1,11 @@
-import type { RequestHandler, RequestParamHandler } from "express";
+import type { NextFunction } from "express";
+import type { Request } from "../types/Request.js";
 import { ForbiddenError, UnauthorizedError } from "../types/Error.js";
 
-export const restrictTo = <TParams, TRes, TReq, TQuery>(
+export const restrictTo = <TRes, TReq extends Request>(
   roles: ("user" | "admin")[],
-): RequestHandler<TParams, TRes, TReq, TQuery> => {
-  return (req, _res, next) => {
+) => {
+  return (req: TReq, _res: TRes, next: NextFunction) => {
     if (!req.user) {
       throw new UnauthorizedError("User not authenticated");
     }
