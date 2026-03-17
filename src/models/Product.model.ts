9c8a4fdfd85f11482a9970ae5ae3706b98fe4628
stastone <StanislauMarudau@coherentsolutions.com>
2026-03-17 09:58:44 +0200

    feat: remove redundant constructors

--- src/models/Product.model.ts
+++ src/models/Product.model.ts
@@ -10,13 +10,13 @@ import { Category } from "./Category.model.js";
 @Entity()
 export class Product {
   @PrimaryGeneratedColumn("uuid")
-  public readonly id: string;
+  public readonly id!: string;
 
   @Column()
-  public name: string;
+  public name!: string;
 
   @Column()
-  public price: number;
+  public price!: number;
 
   @Column({ type: "text", nullable: true })
   public description?: string | null;
@@ -27,20 +27,4 @@ export class Product {
 
   @Column()
   public categoryId!: string;
-
-  constructor(
-    id: string,
-    name: string,
-    price: number,
-    description?: string | null,
-  ) {
-    this.id = id;
-    this.name = name;
-    this.price = price;
-    this.description = description;
-  }
-
-  public getProductInfo(): string {
-    return `Product ID: ${this.id}, Name: ${this.name}, Price: ${this.price}, Description: ${this.description}, Category: ${this.category}`;
-  }
 }
--- src/models/User.model.ts
+++ src/models/User.model.ts
@@ -4,40 +4,20 @@ import type { UserType } from "../schemas/User.schema.js";
 @Entity()
 export class User implements UserType {
   @PrimaryGeneratedColumn("uuid")
-  public readonly id: string;
+  public readonly id!: string;
 
   @Column()
-  public name: string;
+  public name!: string;
 
   @Column()
-  public email: string;
+  public email!: string;
 
   @Column()
-  public password: string;
+  public password!: string;
 
   @Column()
   public role: "user" | "admin" = "user";
 
   @Column({ type: "text", nullable: true })
   public refreshToken: string | null = null;
-
-  constructor(
-    id: string,
-    name: string,
-    email: string,
-    password: string,
-    role: "user" | "admin" = "user",
-    refreshToken?: string,
-  ) {
-    this.id = id;
-    this.name = name;
-    this.email = email;
-    this.password = password;
-    this.role = role;
-    if (refreshToken) this.refreshToken = refreshToken;
-  }
-
-  public getUserInfo(): string {
-    return `User ID: ${this.id}, Name: ${this.name}, Email: ${this.email}, Role: ${this.role}`;
-  }
 }
