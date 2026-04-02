46e799feb2fc1e7f9c6cfb7f70bade0cb64b090e
stastone <StanislauMarudau@coherentsolutions.com>
2026-04-02 22:43:01 +0300

    feat(UserPreferences): controller

---
+++ src/controllers/analytics/analytics.controller.ts
@@ -0,0 +1,29 @@
+import type { RequestHandler } from "express";
+import { BaseController, type ResponsePayload } from "../base.controller.js";
+import type UserPreferenceService from "../../services/user-preference.service.js";
+import type { UserPreferences } from "../../models/UserPreferences.model.js";
+
+class AnalyticsController extends BaseController {
+  private readonly _userPreferencesService: UserPreferenceService;
+
+  constructor(userPreferencesService: UserPreferenceService) {
+    super();
+    this._userPreferencesService = userPreferencesService;
+  }
+  public getUserPreferencesRequestHandler: RequestHandler<
+    {
+      userId: string;
+      limit: number;
+    },
+    ResponsePayload<UserPreferences[]>,
+    null
+  > = async (req, res) => {
+    const { userId, limit } = req.params;
+    const userPreferences =
+      await this._userPreferencesService.getUserPreferences({ userId, limit });
+
+    this.ok(res, userPreferences);
+  };
+}
+
+export default AnalyticsController;
