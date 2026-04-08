367756ee7e97f198bc0ac2fdc488162884baf5d9
stastone <StanislauMarudau@coherentsolutions.com>
2026-04-08 21:39:31 +0300

    feat: hide analytics route behind feature flag

--- src/routers/analytics.routes.ts
+++ src/routers/analytics.routes.ts
@@ -3,14 +3,17 @@ import AnalyticsController from "../controllers/analytics/analytics.controller.j
 import UserPreferenceService from "../services/user-preference.service.js";
 import { Wishlist } from "../models/Wishlist.model.js";
 import { CartCheckoutSession } from "../models/CartCheckoutSession.model.js";
+import { FeatureFlag } from "../models/FeatureFlag.model.js";
 import { mongoDataSource } from "../DataSource.js";
 import { Cart } from "../models/Cart.model.js";
+import { checkFeatureFlag } from "../middlewares/checkFeatureFlag.js";
 
 const analyticsRouter = Router();
 const cartRepository = mongoDataSource.getMongoRepository(Cart);
 const wisthlistRepository = mongoDataSource.getMongoRepository(Wishlist);
 const cartCheckoutSessioinRepository =
   mongoDataSource.getMongoRepository(CartCheckoutSession);
+const featureFlagRepository = mongoDataSource.getMongoRepository(FeatureFlag);
 
 const userPreferencesService = new UserPreferenceService(
   cartRepository,
@@ -22,6 +25,10 @@ const analyticsController = new AnalyticsController(userPreferencesService);
 
 analyticsRouter
   .route("/user-preferences")
-  .get(analyticsController.getUserPreferencesRequestHandler);
+
+  .get(
+    checkFeatureFlag(featureFlagRepository, "user-preferences-analytics"),
+    analyticsController.getUserPreferencesRequestHandler,
+  );
 
 export default analyticsRouter;
